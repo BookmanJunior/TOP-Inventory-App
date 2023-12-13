3b891bbd55acd9d8d485145b76f79c5a1ca18136
Azam Gulov <gulovazam@gmail.com>
2023-12-13 21:24:13 +0500

    Change index route
    remove users route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const gameController = require("../controller/gameController");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get("/", gameController.index);
 
 module.exports = router;
