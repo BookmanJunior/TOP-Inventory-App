a794cea97ec55a0afb22a3ae16a02455b9ebee6b
Azam Gulov <gulovazam@gmail.com>
2023-12-13 21:25:24 +0500

    Change url getter

--- models/game.js
+++ models/game.js
@@ -14,7 +14,7 @@ const gameSchema = new Schema({
 });
 
 gameSchema.virtual("url").get(function () {
-  return `/game/${this._id}`;
+  return `/games/${this._id}`;
 });
 
 module.exports = mongoose.model("Game", gameSchema);
