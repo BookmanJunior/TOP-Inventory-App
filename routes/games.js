d3681848656ec4febd03c2177a8717d7ad9c1890
Azam Gulov <gulovazam@gmail.com>
2023-12-17 23:25:56 +0500

    Add edit get route

--- routes/games.js
+++ routes/games.js
@@ -10,4 +10,6 @@ router.get("/game/create", gameController.game_form_get);
 
 router.post("/game/create", gameController.game_form);
 
+router.get("/:id/edit", gameController.game_form_edit_get);
+
 module.exports = router;
